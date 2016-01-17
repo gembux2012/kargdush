026b6c868c4eb8650250565c68b66177102eb573
alex <gembux2012@gmail.com>
2016-01-17 10:15:38 +0600

    init3

--- protected/Modules/Pages/Migrations/m_1426906516_createPages.php
+++ protected/Modules/Pages/Migrations/m_1426906516_createPages.php
@@ -10,24 +10,26 @@ class m_1426906516_createPages
 
     public function up()
     {
-        $this->createTable('pages',
-            [
-                'title' => [
-                    'type' => 'string',
-                    'length' => 1024,
+        if (!$this->existsTable('__users')) {
+            $this->createTable('pages',
+                [
+                    'title' => [
+                        'type' => 'string',
+                        'length' => 1024,
+                    ],
+                    'url' => [
+                        'type' => 'string',
+                    ],
+                    'text' => [
+                        'type' => 'text',
+                        'length' => 'big',
+                    ],
                 ],
-                'url' => [
-                    'type' => 'string',
+                [
                 ],
-                'text' => [
-                    'type' => 'text',
-                    'length' => 'big',
-                ],
-            ],
-            [
-            ],
-            ['tree']
-        );
+                ['tree']
+            );
+        }
     }
 
 
